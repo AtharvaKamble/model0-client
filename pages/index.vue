7b0391075c3256415dadf98808ed100e63b9816b
AtharvaKamble <atharvadkamble00@gmail.com>
2024-10-19 00:46:15 -0400

    add: landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,29 @@
 <template>
   <section class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="text-center">
-      <h1 class="text-8xl font-bold text-gray-900 mb-4">
+      <h1 class="text-9xl font-bold text-gray-900 mb-4">
         {{ appName }}
       </h1>
       <p class="text-xl text-gray-600">
         {{ description }}
       </p>
+      <div class="mt-10 flex justify-center gap-2">
+        <NuxtLink to="/login" >
+          <button class="px-4 bg-neutral-800 text-white rounded hover:bg-neutral-700 transition font-bold">take me to dashboard</button>
+        </NuxtLink>
+
+        <NuxtLink to="/signup" >
+        <button class="px-4 bg-neutral-800 text-white rounded hover:bg-neutral-700 transition font-bold">i am new to model-0</button>
+
+        </NuxtLink>
+      </div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-const appName = ref('Model0');
-const description = ref('some description')
+const appName = ref('model-0');
+const description = ref('MLaaS platform to train and tune your ML, GPT models and use them in your applications ASAP')
 
 export default {
 
