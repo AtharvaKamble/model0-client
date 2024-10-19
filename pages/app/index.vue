355ab8d492e69259e26e89de1270157576b0bf04
AtharvaKamble <atharvadkamble00@gmail.com>
2024-10-19 00:31:45 -0400

    add: tailwind and global font

---
+++ pages/app/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>This is from the App page</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="text-center">
+      <h1 class="text-8xl font-bold text-gray-900 mb-4">
+        {{ appName }}
+      </h1>
+      <p class="text-xl text-gray-600">
+        {{ description }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+const appName = ref('Model0');
+const description = ref('some description')
+
+export default {
+
+
+  setup() {
+    return {
+        appName,
+      description
+    }
+  }
+}
+
+</script>
